98eccb3be68b152a76e1147bdf8b46a43d5fb4ae
Cepharum GmbH <info@cepharum.de>
2014-01-06 12:07:44 +0100

    revising pathname resolver
    adding new resolver modules featuring support for looking up URL's
    pathname part by part to stop on first encountered file (thus supporting
    URL pathnames with the executed script's filename in the mid instead of
    last position)

--- core/main.js
+++ core/main.js
@@ -26,46 +26,43 @@ var REWRITE     = require( "rewrite" );
 var REDIRECT    = require( "redirect" );
 var OS          = require( "os_tools" );
 var HTTP_PARSER = require( "http_parser" );
+var RESOLVER    = require( "resolver" );
 
 // ----------------------------------------------------------------------------
 
+var webRoot = PATH.resolve( __dirname, CONFIG.webRoot );
+var cgiRoot = PATH.resolve( webRoot, CONFIG.cgiRoot || "." );
+
 /**
- * Maps pathname of a request's URL into absolute pathname of local file.
+ * Chooses one of the configured root folders (webRoot or cgiRoot) depending
+ * on URL starting with configured CGI prefix or not.
  *
  * @param context {Object} context of request
- * @param urlPathname {String} pathname extracted from request's URL
- * @return {String} absolute pathname of a file (though might be missing)
+ * @param url {URL} parsed URL instance
+ * @return {String} absolute pathname of root folder to use on looking for file
+ *                  matching request
  */
 
-function requestUrlToPathname( context, urlPathname )
+function selectRootFolder( context, url )
 {
-	var filePathname;
-
 	var confPrefix = CONFIG.cgiPrefix;
-	if ( urlPathname.substr( 0, confPrefix.length ) == confPrefix )
-	{
-		// select CGI script folder to be web root of current request
-		filePathname = PATH.resolve( CONFIG.webRoot, CONFIG.cgiRoot || "." );
+	var pathname = url.pathname;
+	var isCgi = ( pathname.substr( 0, confPrefix.length ) == confPrefix );
 
+	if ( isCgi )
+	{
 		// remove prefix from URL
-		urlPathname = urlPathname.substr( confPrefix.length );
+		url.pathname = pathname.substr( confPrefix.length );
 
 		// set mark to execute file rather than retrieving its content
 		context.blnExecute = true;
 	}
-	else
-	{
-		// select configured web root
-		filePathname = CONFIG.webRoot;
-	}
 
-	// compile pathname of requested file
-	return PATH.resolve( __dirname, filePathname, urlPathname.replace( /^\/+/, "" ).replace( /(^|\/)\./g, "<invalid-dot>" ) );
+	// choose one of the two available root folders
+	return isCgi ? cgiRoot : webRoot;
 }
 
 // ----------------------------------------------------------------------------
-
-var fncStatter = CONFIG.followSymlinks ? FS.stat : FS.lstat;
 
 OS.normalizeRunAsUser( function( runAsUser )
 {
@@ -139,197 +136,222 @@ OS.normalizeRunAsUser( function( runAsUser )
 			// parse its URL
 			var url = URL.parse( request.url, false );
 
-			// derive pathname of local file selected by URL
-			var pathname = requestUrlToPathname( ctx, url.pathname );
+			// choose root folder
+			var basedir = selectRootFolder( ctx, url );
 
-			LOG.debug( "%s: %s is accessing %s", ctx.index, url.pathname, pathname );
+			LOG.debug( "%s: %s is addressing resource in %s %s", ctx.index, url.pathname, ctx.blnExecute ? "cgiRoot" : "webRoot", basedir );
 
-			FS.exists( pathname, function( exists ) {
-				if ( !exists )
+			// look for matching file
+			RESOLVER.resolvePathname( basedir, url.pathname, !ctx.blnExecute, function( error, pathname, stat )
+			{
+				if ( error || !pathname )
 				{
-					LOG.error( "%s: no such file or directory: %s", ctx.index, pathname );
+					/*
+					 * failed to map pathname of request URL into local file's
+					 * pathname to retrieve or execute
+					 */
 
-					ctx.renderException( {
-						status: 404,
-						title: "File not found",
-						text: "Requested file isn't available."
+					LOG.error( "%s: %s: %s", ctx.index, error.message, pathname || url.pathname );
+
+					switch ( error.type )
+					{
+						case "notfound" :
+							ctx.renderException( {
+								status: 404,
+								title: "Not Found",
+								text: "Requested file does not exist or is not available."
+							} );
+							break;
+
+						case "invalid" :
+							ctx.renderException( {
+								status: 400,
+								title: "Bad Request",
+								text: "Your request has been rejected due to invalid pathname."
+							} );
+							break;
+
+						case "failed" :
+							ctx.renderException( {
+								status: 403,
+								title: "Forbidden",
+								text: "You mustn't access requested file or script."
+							} );
+							break;
+
+						default :
+							ctx.renderException( {
+								status: 500,
+								title: "Internal Error",
+								text: "Processing your request failed due to server-side malfunction."
+							} );
+					}
+				}
+				else if ( ctx.blnExecute )
+				{
+					/*
+					 * request is selecting available script file to execute
+					 */
+
+					LOG.info( "%s: executing %s", ctx.index, pathname );
+
+					var script = CHILD.spawn( pathname, [], {
+						cwd: PATH.dirname( pathname ),
+						uid: runAsUser || stat.uid,
+						stdio: "pipe",
+						env: {
+							CONTENT_LENGTH: request.headers["content-length"] || 0,
+							CONTENT_TYPE: request.headers["content-type"],
+							DOCUMENT_ROOT: CONFIG.webRoot || "/tmp",
+							GATEWAY_INTERFACE: "1.1",
+							HTTP_ACCEPT: request.headers["accept"],
+							HTTP_ACCEPT_CHARSET: request.headers["accept-charset"],
+							HTTP_ACCEPT_ENCODING: request.headers["accept-encoding"],
+							HTTP_ACCEPT_LANGUAGE: request.headers["accept-language"],
+							HTTP_CONNECTION: request.headers["connection"],
+							HTTP_COOKIE: request.headers["cookie"],
+							HTTP_HOST: request.headers["host"],
+							HTTP_REFERER: request.headers["referer"],
+							HTTP_USER_AGENT: request.headers["user-agent"],
+							PATH_INFO: url.pathname,
+							PATH_TRANSLATED: pathname,
+							QUERY_STRING: url.query,
+							REMOTE_ADDRESS: request.socket.remoteAddress,
+							REMOTE_HOST: undefined,
+							REMOTE_IDENT: undefined,
+							REMOTE_PORT: request.socket.remotePort,
+							REMOTE_USER: undefined,
+							REQUEST_METHOD: request.method,
+							REQUEST_URI: request.originalUrl,
+							SCRIPT_FILENAME: pathname,
+							SCRIPT_NAME: url.pathname,
+							SERVER_ADDR: request.socket.localAddress,
+							SERVER_ADMIN: undefined,
+							SERVER_NAME: undefined,
+							SERVER_PORT: request.socket.localPort,
+							SERVER_PROTOCOL: "HTTP/" + request.httpVersion,
+							SERVER_SIGNATURE: undefined,
+							SERVER_SOFTWARE: undefined,
+						}
+					} );
+
+					// render exception on failed invocation of script
+					script.on( "error", function( error )
+					{
+						LOG.error( "%s: failed to execute %s: %s", ctx.index, pathname, error );
+
+						ctx.renderException( {
+							status: 500,
+							title: "Internal Error",
+							text: "The server encountered error on trying to process your request."
+						} );
+					} );
+
+					// capture script's output on stderr to log it line by line
+					script.stderr.on( "data", function( chunk )
+					{
+						chunk.toString( ).split( /\n/ ).forEach( function( line )
+						{
+							LOG.debug( "%s: %s", ctx.index, line );
+						} );
+					} );
+
+
+					// pass request data to script on its stdin
+					request.pipe( script.stdin );
+
+
+					/*
+					 * capture output of script on stdout considered very similar to HTTP message
+					 */
+
+					var parser = HTTP_PARSER.createParser( true, function( headers )
+					{
+						// set response header
+						var status = parseInt( headers.status ) || 200;
+						delete headers.status;
+
+						LOG.debug( "%s: script requests HTTP status %s", ctx.index, status );
+						response.writeHead( status, headers );
+
+						// parser might have collected some leading part of body
+						// -> don't miss to pass it to client
+						response.write( this.rawBody() );
+
+						// and pipe all succeeding output of script into response directly
+						script.stdout.pipe( response );
+
+						// drop reference on parser as it won't be used
+						// for parsing anymore
+						parser = undefined;
+					} );
+
+					script.stdout.on( "data", function( chunk )
+					{
+						// feed HTTP message parser unless disabled before
+						parser && parser.feed( chunk );
+					} );
+
+
+					/*
+					 * log statistical information on script exiting
+					 */
+
+					var exitCode = null;
+					var finished = false;
+
+					function logOnFinish( )
+					{
+						if ( exitCode && finished )
+						{
+							LOG.info( "%s: script exited with %d (signal %s)", ctx.index, exitCode[0], exitCode[1] );
+							LOG.debug( "%s: in %s, out %s", ctx.index, request.socket.bytesRead, request.socket.bytesWritten );
+						}
+					}
+
+					script.on( "exit", function( code, signal )
+					{
+						exitCode = [ code, signal ];
+						logOnFinish( );
+					} );
+
+					script.stdout.on( "end", function( )
+					{
+						finished = true;
+						logOnFinish( );
 					} );
 				}
 				else
 				{
-					fncStatter( pathname, function( error, stat )
-					{
-						if ( error )
-						{
-							LOG.error( "%s: failed to stat %s: %s", ctx.index, pathname, error );
+					/*
+					 * request is selecting available file to retrieve as-is
+					 */
 
-							ctx.renderException( {
-								status: 403,
-								title: "Forbidden",
-								text: "Requested file isn't available."
-							} );
-						}
-						else if ( !stat.isFile() )
-						{
-							LOG.error( "%s: not a file: %s", ctx.index, pathname );
+					LOG.debug( "%s: retrieving file %s", ctx.index, pathname );
 
-							ctx.renderException( {
-								status: 403,
-								title: "Forbidden",
-								text: "Requested file isn't available."
-							} );
-						}
-						else if ( ctx.blnExecute )
-						{
-							// - file exists and is file
-							// - CGI prefix was matching
-							// --> execute file considered CGI script
-							LOG.info( "%s: executing %s", ctx.index, pathname );
+					// retrieve content of selected and available file
+					var ext  = PATH.extname( pathname ).substr( 1 );
+					var map  = CONFIG.mimeTypes;
+					var mime = ( ext in map ? map[ext] : map["*"] ) || "application/octet-stream";
 
-							var script = CHILD.spawn( pathname, [], {
-								cwd: PATH.dirname( pathname ),
-								uid: runAsUser || stat.uid,
-								stdio: "pipe",
-								env: {
-									CONTENT_LENGTH: request.headers["content-length"] || 0,
-									CONTENT_TYPE: request.headers["content-type"],
-									DOCUMENT_ROOT: CONFIG.webRoot || "/tmp",
-									GATEWAY_INTERFACE: "1.1",
-									HTTP_ACCEPT: request.headers["accept"],
-									HTTP_ACCEPT_CHARSET: request.headers["accept-charset"],
-									HTTP_ACCEPT_ENCODING: request.headers["accept-encoding"],
-									HTTP_ACCEPT_LANGUAGE: request.headers["accept-language"],
-									HTTP_CONNECTION: request.headers["connection"],
-									HTTP_COOKIE: request.headers["cookie"],
-									HTTP_HOST: request.headers["host"],
-									HTTP_REFERER: request.headers["referer"],
-									HTTP_USER_AGENT: request.headers["user-agent"],
-									PATH_INFO: url.pathname,
-									PATH_TRANSLATED: pathname,
-									QUERY_STRING: url.query,
-									REMOTE_ADDRESS: request.socket.remoteAddress,
-									REMOTE_HOST: undefined,
-									REMOTE_IDENT: undefined,
-									REMOTE_PORT: request.socket.remotePort,
-									REMOTE_USER: undefined,
-									REQUEST_METHOD: request.method,
-									REQUEST_URI: request.originalUrl,
-									SCRIPT_FILENAME: pathname,
-									SCRIPT_NAME: url.pathname,
-									SERVER_ADDR: request.socket.localAddress,
-									SERVER_ADMIN: undefined,
-									SERVER_NAME: undefined,
-									SERVER_PORT: request.socket.localPort,
-									SERVER_PROTOCOL: "HTTP/" + request.httpVersion,
-									SERVER_SIGNATURE: undefined,
-									SERVER_SOFTWARE: undefined,
-								}
-							} );
-
-							// render exception on failed invocation of script
-							script.on( "error", function( error )
-							{
-								LOG.error( "%s: failed to execute %s: %s", ctx.index, pathname, error );
-
-								ctx.renderException( {
-									status : 500,
-									title : "Internal Error",
-									text : "The server encountered error on trying to process your request."
-								} );
-							} );
-
-							// capture script's output on stderr to log it line by line
-							script.stderr.on( "data", function( chunk )
-							{
-								chunk.toString( ).split( /\n/ ).forEach( function( line )
-								{
-									LOG.debug( "%s: %s", ctx.index, line );
-								} );
-							} );
-
-							var parser = HTTP_PARSER.createParser( true, function( headers )
-							{
-								// set response header
-								var status = parseInt( headers.status ) || 200;
-								delete headers.status;
-
-								LOG.debug( "%s: script requests HTTP status %s", ctx.index, status );
-								response.writeHead( status, headers );
-
-								// parser might have collected some leading part of body
-								// -> don't miss to pass it to client
-								response.write( this.rawBody() );
-
-								// and pipe all succeeding output of script into response directly
-								script.stdout.pipe( response );
-
-								// drop reference on parser as it won't be used
-								// for parsing anymore
-								parser = undefined;
-							} );
-
-							script.stdout.on( "data", function( chunk )
-							{
-								parser && parser.feed( chunk );
-							} );
-
-
-							// add further informational log entries on script exiting
-							var exitCode = null;
-							var finished = false;
-
-							function logOnFinish( )
-							{
-								if ( exitCode && finished )
-								{
-									LOG.info( "%s: script exited with %d (signal %s)", ctx.index, exitCode[0], exitCode[1] );
-									LOG.debug( "%s: in %s, out %s", ctx.index, request.socket.bytesRead, request.socket.bytesWritten );
-								}
-							}
-
-							script.on( "exit", function( code, signal )
-							{
-								exitCode = [ code, signal ];
-								logOnFinish( );
-							} );
-
-							script.stdout.on( "end", function( )
-							{
-								finished = true;
-								logOnFinish( );
-							} );
-
-
-							// pipe any request body data into stdin of script
-							request.pipe( script.stdin );
-						}
-						else
-						{
-							// retrieve content of selected and available file
-							var ext  = PATH.extname( pathname ).substr( 1 );
-							var map  = CONFIG.mimeTypes;
-							var mime = ( ext in map ? map[ext] : map["*"] ) || "application/octet-stream";
-
-							response.writeHead( 200, {
-								"Content-Type": mime,
-							} );
-
-							try
-							{
-								FS.createReadStream( pathname ).pipe( response );
-							}
-							catch ( error )
-							{
-								LOG.error( "%s: on retrieving file %s: %s", ctx.index, pathname, error );
-
-								ctx.renderException( {
-									status : 403,
-									title : "Forbidden",
-									text : "Requested file isn't available."
-								} );
-							}
-						}
+					response.writeHead( 200, {
+						"Content-Type": mime,
 					} );
+
+					try
+					{
+						FS.createReadStream( pathname ).pipe( response );
+					}
+					catch ( error )
+					{
+						LOG.error( "%s: on retrieving file %s: %s", ctx.index, pathname, error );
+
+						ctx.renderException( {
+							status: 403,
+							title: "Forbidden",
+							text: "Requested file isn't available."
+						} );
+					}
 				}
 			} );
 		}
